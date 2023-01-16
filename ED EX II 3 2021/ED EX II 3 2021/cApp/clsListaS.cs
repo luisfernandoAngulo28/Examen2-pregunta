bcc0a4d5db24ea91afd0e26c76f8279142e3a83a
Fernando Angulo <57889477+luisfernandoAngulo28@users.noreply.github.com>
2023-01-16 09:33:55 -0400

    Ayudantia SA-SD
    Ayudantia SA-SD

---
+++ ED EX II 3 2021/ED EX II 3 2021/cApp/clsListaS.cs
@@ -0,0 +1,35 @@
+﻿/*************************************************************
+Institución:    Facultad de Cs. de la Comutacion - UAGRM
+Materia:        Estructura de Datos I
+Proyecto:       cApp (Proyecto de Clases)
+Descripción:    Implementacion del TAD Listas.
+Creador:        // coloque su nombre aqui.
+Lenguaje:       C#
+*************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cApp 
+{
+    public class clsListaS
+    {
+
+        //ATRIBUTOS DE LA CLASE
+
+
+        //CONSTRUCTOR DE LA CLASE
+
+
+        // MODIFICA EL CONSTRUCTOR DE LA CLASE -- con el fin de inicializar los ATRIBUTOS
+
+
+        //PROPIEDADES DE DESCRIPCION DE ACCESOS -- (get/set) que permite leer o cambiar los campos desde otras clases
+
+
+        //FUNCIONES DE LA CLASE
+
+    }
+}
---
+++ ED EX II 3 2021/ED EX II 3 2021/wApp/FListaS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using cApp;
+
+namespace wApp
+{
+    public partial class FListaS : Form
+    {
+        public FListaS()
+        {
+            InitializeComponent();
+        }
+
+        clsListaS L = new clsListaS();
+
+        private void FcolaC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdCreaS_Click(object sender, EventArgs e)
+        {
+            //Q.Drop();
+            //lblCola.Text = Q.Imprimir();
+            //lblLong.Text = "";
+            //lblRear.Text = "";
+            //lblFront.Text = "";
+            //txtEleme.Text = "";
+        }
+
+        private void cmdAdd_Click(object sender, EventArgs e)
+        {
+            // escriba aqui el codigo
+
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            // 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //colocar aqui el codigo
+        }
+
+        private void cmdFront_Click(object sender, EventArgs e)
+        {
+            //lblFront.Text = Convert.ToString(Q.Frente());
+        }
+
+        private void cmdRear_Click(object sender, EventArgs e)
+        {
+            //lblRear.Text = Convert.ToString(Q.Atras());
+        }
+
+        private void txtEleme_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
